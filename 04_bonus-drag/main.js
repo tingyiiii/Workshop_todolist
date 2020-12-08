f973da296600c38878d83fd5d05dec404a6b3202
Ting <0917355950teresa@gmail.com>
2020-12-08 16:32:03 +0800

    drag-fail

--- 04_bonus-drag/main.js
+++ 04_bonus-drag/main.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function(){
   //source
   circle.addEventListener('drag', function(){
     console.log('drag')
+    circle.remove()
   })
 
   circle.addEventListener('dragstart', function(){
@@ -13,23 +14,23 @@ document.addEventListener("DOMContentLoaded", function(){
 
   circle.addEventListener('dragend', function(){
     console.log('dragend')
-    
   })
 
   // //target
-  // target.addEventListener('dragenter', function(){
-  //   console.log('dragenter target')
-  // })
+  target.addEventListener('dragenter', function(){
+    console.log('dragenter target')
+  })
 
-  // target.addEventListener('dragover', function(){
-  //   console.log('dragover target')
-  // })
+  target.addEventListener('dragover', function(){
+    console.log('dragover target')
+    
+  })
 
-  // target.addEventListener('dragleave', function(){
-  //   console.log('dragleave target')
-  // })
+  target.addEventListener('dragleave', function(){
+    console.log('dragleave target')
+  })
 
-  // target.addEventListener('drop', function(){
-  //   console.log('drop')
-  // })
+  target.addEventListener('drop', function(){
+    console.log('drop')
+  })
 })
