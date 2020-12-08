fd3117b56304c410421df1a0bd6ab424ef60d03b
Ting <0917355950teresa@gmail.com>
2020-12-08 11:38:57 +0800

    task_add

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,18 @@
 // TO DO
+document.addEventListener('DOMContentLoaded', function(){
+  document.querySelector('#addBtn').addEventListener('click', function(){
+    let list = document.querySelector('ul')
+    list.insertAdjacentElement('beforeend', addListItem())
+  })
+})
+
+function addListItem(){
+  let li = document.createElement('li')
+  let span = document.createElement('span')
+  let thing = document.querySelector('#input').value
+  span.classList.add('close')
+  span.innerHTML = 'x'
+  li.innerHTML = thing
+  li.appendChild(span)
+  return li
+}
