43e99a76f6664ee2c5fabd147e9ee2880c25c9cb
Ting <0917355950teresa@gmail.com>
2020-12-08 12:11:16 +0800

    Complete_task_add_checked

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,9 +1,17 @@
 // TO DO
 document.addEventListener('DOMContentLoaded', function(){
+  let list_item = document.querySelectorAll('li')
+
   document.querySelector('#addBtn').addEventListener('click', function(){
     let list = document.querySelector('ul')
     list.insertAdjacentElement('beforeend', addListItem())
   })
+  
+  for (let i = 0; i < list_item.length; i++){
+    list_item[i].addEventListener('click', function(){
+      list_item[i].classList.toggle('checked')
+    })
+  }
 })
 
 function addListItem(){
